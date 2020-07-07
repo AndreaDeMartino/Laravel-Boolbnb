8719155b102b09122b9f8216325a8b264e2e6ba6
Andrea De Martino <ademartino1992@gmail.com>
2020-07-07 17:50:42 +0200

    Closes #1 Models Created.

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\salva\Desktop\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\salva\Desktop\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/andrea/Desktop/boolbnb/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/andrea/Desktop/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
