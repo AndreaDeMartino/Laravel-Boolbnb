f06b189bc190daa57857d2df737186e0b0c320b9
Andrea De Martino <ademartino1992@gmail.com>
2020-07-16 17:10:24 +0200

    Add Map on view

--- resources/views/pages/placeShow.blade.php
+++ resources/views/pages/placeShow.blade.php
@@ -15,7 +15,7 @@
     
     <div class="card my-4 d-flex align-items-center">
         @if(!empty($place->place_img))
-            <img src="{{asset('storage/' . $place->place_img)}}" class="card-img-top" alt="logo" style="width: 18rem; height: 20rem;">
+            <img src="{{asset('storage/' . $place->place_img)}}" class="card-img-top" alt="logo" style="width: 18rem;">
         @else
             <div class="no-image text-danger">No image</div>
         @endif
@@ -27,10 +27,18 @@
         <h5 class="card-text h6">Posti letto: {{$place->num_beds}}</h5>
         <h5 class="card-text h6">Bagni: {{$place->num_baths}}</h5>
         <h5 class="card-text h6">Dimensioni: {{$place->square_m}}</h5>
+        <input type="hidden" name="lat" id="lat" value="{{ $place->lat }}">
+        <input type="hidden" name="long" id="long" value="{{ $place->long }}">
+        <div class="card-text" id="mapid" style=" width: 300px;"></div>
     </div>
 </div>
 
 <h2>Contatta il venditore!</h2>
 @include('shared.sendMessageArea')
 
+
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+    crossorigin=""></script>
+<script src="{{ asset('js/map.js') }}"></script>
 @endsection
