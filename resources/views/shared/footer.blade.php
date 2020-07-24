5e7b8b9fafee36877f37bd4f16cd628f940b2858
Francesco <francescoscarniglia@gmail.com>
2020-07-24 17:49:58 +0200

    Navbar-footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,5 +1,42 @@
-<footer>
-    <div class="bg-white shadow-sm text-center mt-5">Footer</div>
+<footer class="mt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-3 col-lg-3 p-4">
+            <h5>Men&ugrave;</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                
+            </ul>
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 p-4">
+            <h5>BoolBnb</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="#">Prezzi</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Come affittare</a></li>
+            </ul>
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 p-4">
+            <h5>Supporto</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="#">FAQ</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contatti</a></li>
+            </ul>  
+            </div>
+
+            <div class="col-6 col-md-3 col-lg-3 p-4">
+            <h5>Seguici</h5>
+            <ul class="nav">
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-github"></i></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-github"></i></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-github"></i></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-github"></i></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fab fa-github"></i></a></li>
+                
+                
+            </ul>
+            </div>
+        </div>
+    </div>
 </footer>
 </div>
 </body>
