017309691c2d1a265429e3d9cdf2dc24017e60ff
Andrea De Martino <ademartino1992@gmail.com>
2020-07-24 18:44:23 +0200

    Add style on home

--- public/js/app.js
+++ public/js/app.js
@@ -37341,8 +37341,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/francesco/Desktop/progettone_finale/boolbnb/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/francesco/Desktop/progettone_finale/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/andrea/Desktop/boolbnb/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/andrea/Desktop/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
